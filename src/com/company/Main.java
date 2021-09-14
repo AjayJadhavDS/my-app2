7e1ca3fbe7ec136b2a3df40288abaaaf0977ea5a
Ajay Jadhav <Ajay.Jadhav@niceactimize.com>
2021-09-14 15:40:51 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import com.company.model.Product;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Product product = new Product("Ajay");
+        System.out.println(product.getName());
+    }
+}
